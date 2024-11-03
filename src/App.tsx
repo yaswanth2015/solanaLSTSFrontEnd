84fd04b7fed8a32169dc123946ed09dcae97fb3a
yaswanth2015 <50266405+yaswanth2015@users.noreply.github.com>
2024-11-03 06:30:40 +0000

    Added Transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
+import { ConnectionProvider, useConnection, useWallet, WalletProvider } from "@solana/wallet-adapter-react"
+import { WalletConnectButton, WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui"
+import { clusterApiUrl, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js"
+import { useMemo, useState } from "react"
+import '@solana/wallet-adapter-react-ui/styles.css';
+const receiverPubKey = new PublicKey("9fhanagv1DHMtRCXUuKfJgzxaEJL9qn35Lc38c6ApnC1")
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(()=> clusterApiUrl(network), [network])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider >
+          <Dapp />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+      
     </>
   )
+}
+
+function Dapp() {
+  return <>
+  <ConnectWallet />
+  <TransactionBlock />
+  </>
+}
+
+function ConnectWallet() {
+  return <>
+    <div style={{display: "flex", justifyContent: "space-between"}}>
+      <WalletMultiButton />
+      <WalletDisconnectButton />
+    </div>
+  </>
+}
+
+function TransactionBlock() {
+  const [sol, setLamports] = useState("")
+  const wallets = useWallet()
+  const {connection} = useConnection()
+
+  function submit() {
+    console.log("insode submit")
+    const transaction = new Transaction()
+    const amount = (sol as unknown) as number
+    console.log(wallets.publicKey)
+    if (Number.isNaN(amount)) {
+      return
+    }
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: wallets.publicKey as PublicKey,
+        toPubkey: receiverPubKey,
+        lamports: amount*LAMPORTS_PER_SOL
+      })
+    )
+    wallets.sendTransaction(transaction, connection).then((value)=>{
+      console.log(value)
+      alert(`amount transfered succefully`)
+    })
+  }
+
+  return <>
+  <div>
+    <input placeholder="Enter Lamports" onChange={(e)=>{setLamports(e.target.value)}}></input>
+    <button onClick={()=> {submit()}}>Send</button>
+  </div>
+  </>
 }
 
 export default App
